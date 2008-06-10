42ef41c0d2975bc7f464f4900d886a7ce3665fde
James Dixon <jdixon@pentaho.com>
2008-06-10 20:43:08 +0000

    Made the action definition class list pluggable
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/trunk/pentaho-actionsequence-dom@26721 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/actionsequence/dom/actions/ActionFactory.java
+++ src/org/pentaho/actionsequence/dom/actions/ActionFactory.java
@@ -1,73 +1,105 @@
 package org.pentaho.actionsequence.dom.actions;
 
+import java.io.InputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 
 public class ActionFactory {
-  public static final Class[] AVAILABLE_ACTIONS = new Class[] {
-    JFreeReportAction.class,
-    JasperReportAction.class,
-    BirtReportAction.class,
-    BarChartAction.class,
-    PieChartAction.class,
-    LineChartAction.class,
-    AreaChartAction.class,
-    SqlQueryAction.class,
-    SqlDataAction.class,
-    SqlExecuteAction.class,
-    MdxQueryAction.class,
-    HQLQueryAction.class,
-    XQueryAction.class,
-    XQueryConnectionAction.class,
-    PivotViewAction.class,
-    ResultSetCompareAction.class,
-    ListSchedJobsAction.class,
-    ResumeSchedulerAction.class,
-    SchedulerStatusAction.class,
-    SuspendSchedulerAction.class,
-    SuspendScheduledJobAction.class,
-    ResumeScheduledJobAction.class,
-    StartScheduledJobAction.class,
-    DeleteScheduledJobAction.class,
-    KettleTransformAction.class,
-    KettleJobAction.class,
-    FormatMsgAction.class,
-    CopyParamAction.class,
-    ContentOutputAction.class,
-    PrintParamAction.class,
-    PrintMapValsAction.class,
-    HelloWorldAction.class,
-    MQLAction.class,
-    JMSAction.class,
-    JFreeReportGenAction.class,
-    EmailAction.class,
-    JavascriptAction.class,
-    PrinterAction.class,
-    SharkWorkflowAction.class,
-    ReceiptAuditAction.class,
-    TemplateMsgAction.class,
-    SecureFilterAction.class,
-    SubActionAction.class,
-    SqlConnectionAction.class,
-    MdxConnectionAction.class,
-    HQLConnectionAction.class
-  };
+
+  public static List<Class> pluginActions = new ArrayList<Class>();
+  
+  public static String PLUGIN_XML_FILENAME = "pentaho_platform_plugin.xml";
+  public static String PLUGIN_ROOT_NODE = "pentaho-plugin";
+  public static String PLUGIN_ACTION_DEFINITION_NODE = "action-definition";
+  
+  protected static boolean pluginsLoaded = false;
+  
+  protected static void loadPlugins() {
+	  try {
+		  // see if we have any pentaho_action_plugin.xml files in the root of the classpath
+		  ActionFactory factory = new ActionFactory();
+		  Enumeration<URL> enumer = factory.getClass().getClassLoader().getResources( PLUGIN_XML_FILENAME );
+		  
+		  // we might have multiple documents
+		  while( enumer.hasMoreElements() ) {
+			  URL url = enumer.nextElement();
+			  // make sure a failure for one resource does not affect any other ones
+			  try {
+				  Object obj = url.getContent();
+				  if( obj instanceof InputStream ) {
+					  // we have an input stream, it should give us an xml document
+					  InputStream in = (InputStream) obj;
+					  StringBuilder str = new StringBuilder();
+					  byte buffer[] = new byte[4096];
+					  int n = in.read( buffer );
+					  while( n != -1 ) {
+						  str.append( new String( buffer, 0, n, "UTF-8" ) );
+						  n = in.read( buffer );
+					  }
+					  // we have the text now generate a DOM
+					  Document doc = DocumentHelper.parseText( str.toString() );
+					  if( doc != null ) {
+						  // look for nodes 
+						  List nodes = doc.selectNodes( PLUGIN_ROOT_NODE+"/"+PLUGIN_ACTION_DEFINITION_NODE );
+						  Iterator it = nodes.iterator();
+						  while( it.hasNext() ) {
+							  // make sure that one failed class will not affect any others
+							  try {
+								  // pull the class name from each node
+								  Element node = (Element) it.next();
+								  String className = node.getText();
+								  // load the class
+							      Class componentClass = Class.forName(className.trim());
+							      // add the class to the plugin list
+							      pluginActions.add( componentClass );
+							  } catch (Exception e) {
+								  // TODO log this
+								  e.printStackTrace();
+							  }
+						  }
+					  }
+				  }
+			  } catch (Exception e) {
+				  // TODO log this
+				  e.printStackTrace();
+			  }
+		  }
+	  } catch (Exception e) {
+		  // TODO log this
+		  e.printStackTrace();
+	  }
+	  pluginsLoaded = true;
+  }
   
   public static ActionDefinition getActionDefinition(Element actionDefDomElement, IActionParameterMgr actionInputProvider) {
     ActionDefinition actionDefinition = null;
-    for (int i = 0; (i < AVAILABLE_ACTIONS.length) && (actionDefinition == null); i++) {
-      try {
-        Method acceptElementMethod = AVAILABLE_ACTIONS[i].getMethod("accepts", new Class[]{Element.class});
-        if (Boolean.TRUE.equals(acceptElementMethod.invoke(null, new Object[]{actionDefDomElement}))) {
-          Constructor constructor = AVAILABLE_ACTIONS[i].getConstructor(new Class[]{Element.class, IActionParameterMgr.class});
-          actionDefinition = (ActionDefinition)constructor.newInstance(new Object[]{actionDefDomElement, actionInputProvider});
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
+
+    if( !pluginsLoaded ) {
+        loadPlugins();
     }
+
+    // TODO a map would improve performance here
+    for (int i = 0; (i < pluginActions.size()) && (actionDefinition == null); i++) {
+        try {
+          Method acceptElementMethod = pluginActions.get( i ).getMethod("accepts", new Class[]{Element.class});
+          if (Boolean.TRUE.equals(acceptElementMethod.invoke(null, new Object[]{actionDefDomElement}))) {
+            Constructor constructor = pluginActions.get( i ).getConstructor(new Class[]{Element.class, IActionParameterMgr.class});
+            actionDefinition = (ActionDefinition)constructor.newInstance(new Object[]{actionDefDomElement, actionInputProvider});
+          }
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+
     if (actionDefinition == null) {
       actionDefinition = new ActionDefinition(actionDefDomElement, actionInputProvider);
     }
